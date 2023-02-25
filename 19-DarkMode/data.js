8efcabc91883eb46b5d58ef4b1e1b80acac535a1
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-02-25 17:47:32 -0300

    DarkMode

---
+++ 19-DarkMode/data.js
@@ -0,0 +1,30 @@
+const articles = [
+    {
+        id: 1,
+        title: 'Formas de preparar un Cafè',
+        date: new Date(2020, 9, 4),
+        length: 11,
+        snippet: `a preparación depende del gusto de quien la toma. Sin embargo, el proceso consiste de etapas imprescindibles que pueden mejorar o perjudicar el sabor final de la emblemática bebida, una experta nos da algunas pautas para hacer la mejor bebida.`,
+    },
+    {
+        id: 2,
+        title: 'goodby, clean code',
+        date: new Date(2019, 10, 22),
+        length: 5,
+        snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
+    },
+    {
+        id: 3,
+        title: 'my decade in review',
+        date: new Date(2018, 7, 11),
+        length: 5,
+        snippet: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
+    },
+    {
+        id: 4,
+        title: 'what are the react team principles',
+        date: new Date(2015, 5, 4),
+        length: 5,
+        snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
+    },
+];
